67c22c41744f8ea1dc024a3a3a2a01f57be1a2cd
Yuzer <23052124+yusing@users.noreply.github.com>
2020-08-31 19:13:49 +0800

    Update Example.cpp

--- Example.cpp
+++ Example.cpp
@@ -4,8 +4,8 @@
 #include <vector>
 #include "TypeInfo.hxx"
 template <class>
-struct Test{};
-InstantiateTemplateTypeInfo(Test);
+struct MyStruct{};
+InstantiateTemplateTypeInfo(MyStruct);
 
 using std::cin;
 using std::cout;
@@ -19,42 +19,39 @@ using std::vector;
 
 int main()
 {
-    cout << (GetTypeInfo<int> == GetTypeInfo<const int>) << endl;
-    cout << (GetTypeInfo<int> == GetTypeInfo<int&>) << endl;
-    cout << (GetTypeInfo<int> == GetTypeInfo<int&&>) << endl;
-    cout << (GetTypeInfo<int&> == GetTypeInfo<int&&>) << endl;
-    cout << (GetTypeInfo<int*> == GetTypeInfo<int*&>) << endl;
-    cout << (GetTypeInfo<int*> == GetTypeInfo<int*&&>) << endl;
-    cout << (GetTypeInfo<int*[]> == GetTypeInfo<int**>) << endl;
-    cout << (GetTypeInfo<int> == GetTypeInfo<volatile int>) << endl;
-    cout << (GetTypeInfo<volatile int> == GetTypeInfo<volatile const int>) << endl;
+    /* const, volatile and reference will not be striped so static_assert should succeed. */
+    static_assert(GetTypeInfo<int> != GetTypeInfo<const int>, "");
+    static_assert(GetTypeInfo<int> != GetTypeInfo<int&>, "");
+    static_assert(GetTypeInfo<int> != GetTypeInfo<int&&>, "");
+    static_assert(GetTypeInfo<int&> != GetTypeInfo<int&&>, "");
+    static_assert(GetTypeInfo<int*> != GetTypeInfo<int*&>, "");
+    static_assert(GetTypeInfo<int*> != GetTypeInfo<int*&&>, "");
+    static_assert(GetTypeInfo<int*[]> != GetTypeInfo<int**>, "");
+    static_assert(GetTypeInfo<int> != GetTypeInfo<volatile int>, "");
+    static_assert(GetTypeInfo<volatile int> != GetTypeInfo<volatile const int>, "");
 
-    cout << TypeName(GetTypeInfo<int>) << endl;
-    cout << TypeName(GetTypeInfo<int*>) << endl;
-    cout << TypeName(GetTypeInfo<const int*>) << endl;
-    cout << TypeName(GetTypeInfo<const int**>) << endl;
-    cout << TypeName(GetTypeInfo<const int***>) << endl;
-    cout << TypeName(GetTypeInfo<const int*&>) << endl;
-    cout << TypeName(GetTypeInfo<const int**&>) << endl;
-    cout << TypeName(GetTypeInfo<const int***&>) << endl;
-    cout << TypeName(GetTypeInfo<const int&&>) << endl;
-    cout << TypeName(GetTypeInfo<const int*&&>) << endl;
-    cout << TypeName(GetTypeInfo<const int**&&>) << endl;
-    cout << TypeName(GetTypeInfo<volatile int*>) << endl;
-    cout << TypeName(GetTypeInfo<int const*>) << endl;
-    cout << TypeName(GetTypeInfo<int const volatile*>) << endl;
-    cout << TypeName(GetTypeInfo<int const volatile*[]>) << endl;
-    cout << TypeName(GetTypeInfo<double(*)()>) << endl;
-    cout << TypeName(GetTypeInfo<double(*)(int,int,int)>) << endl;
-    cout << TypeName(GetTypeInfo<Test<Test<int*>&&>*&>) << endl;
-    cout << TypeName(GetTypeInfo<Test<int>&(*)(const Test<int>&)>) << endl;
-    cout << TypeName(GetTypeInfo<Test<int>&(*)(Test<int>&&)>) << endl;
-    cout << TypeName(GetTypeInfo<string>) << endl;
-    cout << TypeName(GetTypeInfo<decltype(cin)>) << endl;
-    cout << TypeName(GetTypeInfo<decltype(cout)>) << endl;
-    cout << TypeName(GetTypeInfo<ifstream>) << endl;
-    cout << TypeName(GetTypeInfo<ofstream>) << endl;
-    cout << TypeName(GetTypeInfo<unordered_map<unsigned int, const string&>>) << endl;
-    cout << TypeName(GetTypeInfo<tuple<int, unsigned, long, float, double>>) << endl;
-    cout << TypeName(GetTypeInfo<size_t(*)(const vector<string>&)>) << endl;
+    cout << TypeName(GetTypeInfo<int>) << endl; // output: "int"
+    cout << TypeName(GetTypeInfo<int*>) << endl; // output: "int*"
+    cout << TypeName(GetTypeInfo<int const*>) << endl; // output: "const int*"
+    cout << TypeName(GetTypeInfo<const int*>) << endl; // output: "const int*"
+    cout << TypeName(GetTypeInfo<const int*&>) << endl; // output: "const int*&"
+    cout << TypeName(GetTypeInfo<const int**&>) << endl; // output: "const int**&"
+    cout << TypeName(GetTypeInfo<const int&&>) << endl; // output: "const int&&"
+    cout << TypeName(GetTypeInfo<const int**&&>) << endl; // output: "const int**&&"
+    cout << TypeName(GetTypeInfo<volatile int*>) << endl; // output: "volatile int*"
+    cout << TypeName(GetTypeInfo<int const volatile*>) << endl; // output: "const volatile int*"
+    cout << TypeName(GetTypeInfo<int const volatile*[]>) << endl; // output: "const volatile int*[]"
+    cout << TypeName(GetTypeInfo<double(*)()>) << endl; // output: "double(*)(void)"
+    cout << TypeName(GetTypeInfo<double(*)(int,int,int)>) << endl; // output: "double(*)(int, int, int)"
+    cout << TypeName(GetTypeInfo<MyStruct<MyStruct<int*>&&>*&>) << endl; // output: "MyStruct<MyStruct<int*>&&>*&"
+    cout << TypeName(GetTypeInfo<MyStruct<int>&(*)(const MyStruct<int>&)>) << endl; // output: "MyStruct<int>&(*)(const MyStruct<int>&)"
+    cout << TypeName(GetTypeInfo<const MyStruct<int>&&(*)(MyStruct<int>&&)>) << endl; // output: "const MyStruct<int>&&(*)(MyStruct<int>&&)"
+    cout << TypeName(GetTypeInfo<string>) << endl; // output: "std::basic_string<char, std::char_traits<char>, std::allocator<char>>"
+    cout << TypeName(GetTypeInfo<decltype(cin)>) << endl; // output: "std::basic_istream<char, std::char_traits<char>>"
+    cout << TypeName(GetTypeInfo<decltype(cout)>) << endl; // output: "std::basic_ostream<char, std::char_traits<char>>"
+    cout << TypeName(GetTypeInfo<ifstream>) << endl; // output: "std::basic_ifstream<char, std::char_traits<char>>"
+    cout << TypeName(GetTypeInfo<ofstream>) << endl; // output: "std::basic_ofstream<char, std::char_traits<char>>"
+    cout << TypeName(GetTypeInfo<unordered_map<unsigned int, const string&>>) << endl; // output: "std::unordered_map<unsigned int, const std::basic_string<char, std::char_traits<char>, std::allocator<char>>&, std::hash<unsigned int>, std::equal_to<unsigned int>, std::allocator<std::pair<const unsigned int, const std::basic_string<char, std::char_traits<char>, std::allocator<char>>&>>>"
+    cout << TypeName(GetTypeInfo<tuple<int, unsigned, long, float, double>>) << endl; // output: "std::tuple<int, unsigned int, long, float, double>"
+    cout << TypeName(GetTypeInfo<size_t(*)(const vector<string>&)>) << endl; // output: "unsigned long long(*)(const std::vector<std::basic_string<char, std::char_traits<char>, std::allocator<char>>, std::allocator<std::basic_string<char, std::char_traits<char>, std::allocator<char>>>>&)"
 }
